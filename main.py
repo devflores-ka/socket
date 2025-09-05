8a7319ae7cf6b8714724c546edab6ada9f9a296c
Luciano Flores Rojas <lucianoflores2022@hotmail.com>
2025-09-04 22:02:33 -0400

    Proyecto iniciado

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Autor: Luciano Flores Rojas
+Fecha: 04/09/2025
+Descripción: Este programa es un servidor de comunicación basado en sockets y arquitectura cliente-servidor.
+Licencia: MIT
+"""
+
+import socket
